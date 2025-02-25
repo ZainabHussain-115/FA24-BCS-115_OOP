97bf151747e13b56a29c93a588b2d3c3dd94a27d
ZAINAB HUSSAIN <zainab.hussain.k1827@gmail.com>
2025-02-25 17:32:11 +0500

    Create Userinfo.java

---
+++ Week3/Userinfo.java
@@ -0,0 +1,20 @@
+class Userinfo{
+	private static String name="Zainab";
+	private static String password="abc123";
+	
+	public static void setName(String var1){
+	this.name=var1;
+	}
+	public static String getName() {
+         return name;
+       }
+
+
+      public static String getPassword() {
+        return password;
+      }
+public static void setPassword(String var2){
+	this.password=var2;
+	}
+ 
+}
