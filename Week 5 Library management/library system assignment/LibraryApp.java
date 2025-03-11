6094d12b1c898347984212cd556f7f7ddcdd44e5
ZAINAB HUSSAIN <zainab.hussain.k1827@gmail.com>
2025-03-11 19:29:10 +0500

    Add files via upload

--- Week 5 Library management/library system assignment/LibraryApp.java
+++ Week 5 Library management/library system assignment/LibraryApp.java
@@ -1,16 +1,7 @@
 public class LibraryApp{
 	public static void main (String[] args){
 
-		Address addressauthor = new Address("120 Street", "Lahore");
-		Address addressincharge = new Address("115 Street", "Karachi");
-		Address addressstaff = new Address("009 Street", "Multan");
-        	Person author = new Person("Zainab", "Writer", addressauthor);
-        	Date publicationDate = new Date(1, 5, 1996);
-       		Book book = new Book("Harry Potter", "123", publicationDate, author);
-        	Person incharge = new Person("Bismah", "Manager", addressincharge);
-        	Person staff = new Person("Asna", "Librarian", addressstaff);
-
-		Library library=new Library("COMSATS library",book,incharge,staff);
+		Library library=new Library("COMSATS library",new Book("Harry Potter", "123", new Date(1,5,1996), new Person("Zainab Hussain","Writer",new Address("120 street","Lahore."))),new Person ("Bismah","Manager",new Address("115 Street","Karachi")),new Person ("Asna","Librarian",new Address("009 Street","Multan")));
 		library.showLibraryDetails();
 
 	}
